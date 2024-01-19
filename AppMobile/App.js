41749072deb968ed23bd3978e38e302947ac0eb8
marionjorda <marionjorda@next-u.fr>
2024-01-19 16:39:08 +0100

    séparation api et projet

--- AppMobile/App.js
+++ AppMobile/App.js
@@ -1,25 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Acceuil from './pages/acceuil/Acceuil.jsx';
-
-
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Appbar } from "react-native-paper";
+import Acceuil from "./pages/acceuil/Acceuil.jsx";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! coucou</Text>
-      <StatusBar style="auto" />
-      <Acceuil />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Appbar.Header style={styles.navbar}>
+          <Appbar.Action icon="menu" onPress={() => {}} color="white" />
+          <Text style={styles.title}><Appbar.Content title="Mademoiselle passepoil" color="white" /></Text>
+          <Appbar.Action icon="cart" onPress={() => {}} color="white" />
+        </Appbar.Header>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Acceuil />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  navbar:{
+    backgroundColor: "#720B21",
   },
 });
--- AppMobile/pages/acceuil/Acceuil.jsx
+++ AppMobile/pages/acceuil/Acceuil.jsx
@@ -5,7 +5,7 @@ import styles from './acceuil.scss';
 
 const Acceuil = () => {
   return (
-    <View style={[styles.container, {backgroundColor:"red"}]}>
+    <View style={styles.container}>
       <Text style={styles.text}>Bonjour, ceci est ma page React Native!</Text>
     </View>
   );
